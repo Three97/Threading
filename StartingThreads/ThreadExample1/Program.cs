e1060380b743bcd9a0289726a06c7f73a4f4d72d
Jason Fedler <Jason.Fedler@garmin.com>
2022-10-10 08:37:06 -0400

    adds solution and Thread Example 1 project using Rider;

---
+++ StartingThreads/ThreadExample1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadExample1
+{
+    class MainClass
+    {
+        private const int REPETITIONS = 1000;
+
+        public static void DoWork()
+        {
+            for (int i = 0; i < REPETITIONS; i++)
+            {
+                Console.Write("B");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            // create thread using thread and thread start constructors
+            // Thread t1 = new Thread(new ThreadStart(DoWork));
+            // t1.Start();
+            
+            // can also do this using just a Thread object
+            // var t2 = new Thread(DoWork);
+            // t2.Start();
+            
+            // can ALSO replace with lambda expression
+            var t3 = new Thread(() => { DoWork(); });
+            t3.Start();
+
+            for (int i = 0; i < REPETITIONS; i++)
+            {
+                Console.Write("A");
+            }
+        }
+    }
+}
